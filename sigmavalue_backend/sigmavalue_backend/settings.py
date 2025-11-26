7c96ccf0e81549aea7764e4099899462cb2dd92e
chyn05 <kumaraman3206@gmail.com>
2025-11-26 19:09:47 +0530

    Update settings.py

--- sigmavalue_backend/sigmavalue_backend/settings.py
+++ sigmavalue_backend/sigmavalue_backend/settings.py
@@ -40,14 +40,14 @@ TEMPLATES = [
         "DIRS": [],
         "APP_DIRS": True,
         "OPTIONS": {
-            "context_processors": [
+            "context_processors": {
                 "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-            ],
-        },
-    },
+            }    
+        }
+    }
 ]
 
 WSGI_APPLICATION = "sigmavalue_backend.wsgi.application"
